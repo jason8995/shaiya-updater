6f2d3b9625647511e38ca2b613ec0a6e18e3991b
kurtekat <142125482+kurtekat@users.noreply.github.com>
2025-02-21 02:00:01 -0500

    Create PathHelper.cs

---
+++ Updater/Helpers/PathHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace Updater.Helpers
+{
+    public static class PathHelper
+    {
+        public static bool IsDirectorySeparator(char value)
+        {
+            return value == '/' || value == '\\';
+        }
+
+        public static string MakePreferred(string path)
+        {
+            var newChar = Path.DirectorySeparatorChar;
+            return path.Replace('/', newChar).Replace('\\', newChar);
+        }
+
+        public static string[] Split(string path)
+        {
+            var separator = new char[] { '/', '\\' };
+            return path.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
